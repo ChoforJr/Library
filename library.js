1bb235d1e0e625d9c7c297271bb240a46acd5200
ChoforJr <choforforsakang@gmail.com>
2024-08-04 02:51:25 +0100

    Added the delete buttons on the book cards
    I made it possible to delete the from display and from the library, all
    with a single button

--- library.js
+++ library.js
@@ -8,51 +8,91 @@ let readStatus = document.querySelector("#readOrNot");
 let addBookButton = document.querySelector("#addBookButton");
 let pushBookButton = document.querySelector("#pushBookButton");
 
+
 const myLibrary = [      
-  {
+ /* {
+    id:0,
     title: 'Harry Porter',
     author: 'J.K Rowley',
     pages: 423,
     readOrNot: 'not read yet'
   },
   {
+    id:1,
     title: 'The All Father',
     author: 'Odin',
     pages: 1345,
     readOrNot: 'read'
-  }];
+  }*/];
 
-function Book(title,author,pages,readOrNot) {
+function Book(id,title,author,pages,readOrNot) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.readOrNot = readOrNot;
 }
 
-function addBookToLibrary(title,author,pages,readOrNot) {
-  Book.call(this,title,author,pages,readOrNot);
+function addBookToLibrary(id,title,author,pages,readOrNot) {
+  Book.call(this,id,title,author,pages,readOrNot);
 }
 
 Object.setPrototypeOf(addBookToLibrary.prototype, Book.prototype);
 
 addBookToLibrary.prototype.info = function () {
-  myLibrary.push({title:this.title,author:this.author,pages:this.pages,readOrNot:this.readOrNot});
+  myLibrary.push({id:this.id,title:this.title,author:this.author,pages:this.pages,readOrNot:this.readOrNot});
 };
 
+function clearInput(){
+  titleName.value = '';
+  authorName.value = '';
+  pagesNum.value = '';
+  readStatus.value = '';
+}
+
 addBookButton.addEventListener("click", ()=>{
-  let newBook = new addBookToLibrary(`${titleName.value}`,`${authorName.value}`,`${pagesNum.value}`,`${readStatus.value}`);
+  let addId = myLibrary.length;
+  let newBook = new addBookToLibrary(addId,`${titleName.value}`,`${authorName.value}`,`${pagesNum.value}`,`${readStatus.value}`);
   newBook.info();
+  clearInput();
 });
 
-/*function pushToDisplay(){
-  
-}
-*/
 pushBookButton.addEventListener("click", ()=>{
   for(let i=0;i<myLibrary.length;i++){
     let bookCard = document.createElement("div");
-    bookCard.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}, ${myLibrary[i].pages} pages, I have ${myLibrary[i].readOrNot}`;
+    let titlePush = document.createElement("h2");
+    let authorPush = document.createElement("p");
+    let pagesPush = document.createElement("p");
+
+    let readStatusPush = document.createElement("button");
+    readStatusPush.classList.add("readCondition")
+
+    let removeButtonPush = document.createElement("button");
+    removeButtonPush.classList.add("remove");
+    removeButtonPush.setAttribute('id',i);
+
+    titlePush.textContent = `${myLibrary[i].title}`;
+    authorPush.textContent = `by ${myLibrary[i].author}`;
+    pagesPush.textContent = `${myLibrary[i].pages} pages`;
+    readStatusPush.textContent = `${myLibrary[i].readOrNot}`;
+    removeButtonPush.textContent = `Remove`;
+
+    bookCard.appendChild(titlePush);
+    bookCard.appendChild(authorPush);
+    bookCard.appendChild(pagesPush);
+    bookCard.appendChild(readStatusPush);
+    bookCard.appendChild(removeButtonPush);
+
     bookCollection.appendChild(bookCard);
+
+    removeButtonPush.addEventListener("click", ()=>{
+      if(myLibrary[i].id===i){
+        delete myLibrary[i]; 
+        bookCollection.removeChild(bookCard);
+        //myLibrary.splice(i,1);
+        //console.log(myLibrary[i]);
+      }
+    });
   }
 });
 
