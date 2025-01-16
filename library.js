82c5864790eeb86b2191f838150eda18590e7bc9
ChoforJr <choforforsakang@gmail.com>
2025-01-16 02:12:03 +0100

    Add JS For validation
    I add custom Javascript validation to the input forms as practice
    of Javascript form validation

--- library.js
+++ library.js
@@ -8,54 +8,119 @@ let readStatus = document.querySelector("#readOrNot");
 let addBookButton = document.querySelector("#addBookButton");
 let pushBookButton = document.querySelector("#pushBookButton");
 
+const titleErrorSpace = document.querySelector("#titleErrorSpace");
 
-const myLibrary = [ /*... */];
+const myLibrary = [
+  /*... */
+];
 
+const titlePattern = /^[^#!$%\^&\*;:{}=_`~()]*$/; // Allows letters, digits, whitespace  and some punctuation only
+
+titleName.addEventListener("input", (event) => {
+  const isValid = titlePattern.test(event.target.value);
+  if (!isValid) {
+    titleErrorSpace.textContent =
+      "Title can only contain letters, digits, whitespace, and some common punctuation (., /-).";
+  } else if (titleName.validity.tooShort) {
+    titleErrorSpace.textContent = "Title must be atleast 4 characters";
+  } else if (titleName.validity.tooLong) {
+    titleErrorSpace.textContent = "Title must be less 20 characters";
+  } else {
+    titleErrorSpace.textContent = "";
+  }
+});
+
+const authorPattern = /^[^\d,\/#!$%\^&\*;:{}=_/`~()]*$/; // Allows letters, whitespace and some punctuation only
+
+authorName.addEventListener("input", (event) => {
+  const isValid = authorPattern.test(event.target.value);
+  if (!isValid) {
+    authorName.setCustomValidity(
+      "Author's name can only contain letters, whitespace, and some common punctuation (.-)."
+    );
+  } else if (authorName.validity.tooShort) {
+    authorName.setCustomValidity("Author's Name must be atleat 4 characters");
+  } else if (authorName.validity.tooLong) {
+    authorName.setCustomValidity("Author's Name must be less 20 characters");
+  } else {
+    authorName.setCustomValidity("");
+  }
+  authorName.reportValidity();
+});
+
+pagesNum.addEventListener("input", () => {
+  if (pagesNum.validity.rangeUnderflow) {
+    pagesNum.setCustomValidity("Book can't be less than 50 pages");
+  } else if (pagesNum.validity.rangeOverflow) {
+    pagesNum.setCustomValidity("Book can't be more than 5000 pages");
+  } else {
+    pagesNum.setCustomValidity("");
+  }
+  pagesNum.reportValidity();
+});
 
 class addBookToLibrary {
-  constructor (id,title,author,pages,readOrNot) {
+  constructor(id, title, author, pages, readOrNot) {
     this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readOrNot = readOrNot;
   }
-  get info (){
-    return myLibrary.push({id:this.id,title:this.title,author:this.author,pages:this.pages,readOrNot:this.readOrNot});
+  get info() {
+    return myLibrary.push({
+      id: this.id,
+      title: this.title,
+      author: this.author,
+      pages: this.pages,
+      readOrNot: this.readOrNot,
+    });
   }
 }
 
-
-function clearInput(){
-  titleName.value = '';
-  authorName.value = '';
-  pagesNum.value = '';
+function clearInput() {
+  titleName.value = "";
+  authorName.value = "";
+  pagesNum.value = "";
   readStatus.checked = false;
 }
 
-addBookButton.addEventListener("click", (event)=>{
+addBookButton.addEventListener("click", (event) => {
   event.preventDefault();
-  
+
   let readShow;
-  if(readStatus.checked){
+  if (readStatus.checked) {
     readShow = "Read";
-  }else{ readShow = "Not read";}
-  
+  } else {
+    readShow = "Not read";
+  }
+
   let addId = myLibrary.length;
 
-  if (titleName.value.trim() === ''||authorName.value.trim() === ''||pagesNum.value.trim() === ''||readStatus.value.trim() === '') {
-    alert('Please fill out the input field!');
+  if (
+    titleName.value.trim() === "" ||
+    authorName.value.trim() === "" ||
+    pagesNum.value.trim() === "" ||
+    readStatus.value.trim() === ""
+  ) {
+    alert("Please fill out the input field!");
     return;
   }
 
-  const newBook = new addBookToLibrary(addId,`${titleName.value}`,`${authorName.value}`,`${pagesNum.value}`,`${readShow}`);
+  const newBook = new addBookToLibrary(
+    addId,
+    `${titleName.value}`,
+    `${authorName.value}`,
+    `${pagesNum.value}`,
+    `${readShow}`
+  );
 
   newBook.info;
   clearInput();
 });
 
-pushBookButton.addEventListener("click", ()=>{
-  for(let i=0;i<myLibrary.length;i++){
+pushBookButton.addEventListener("click", () => {
+  for (let i = 0; i < myLibrary.length; i++) {
     let bookCard = document.createElement("div");
     let titlePush = document.createElement("h2");
     let authorPush = document.createElement("p");
@@ -66,7 +131,7 @@ pushBookButton.addEventListener("click", ()=>{
 
     let removeButtonPush = document.createElement("button");
     removeButtonPush.classList.add("remove");
-    removeButtonPush.setAttribute('id',i);
+    removeButtonPush.setAttribute("id", i);
 
     titlePush.textContent = `${myLibrary[i].title}`;
     authorPush.textContent = `by ${myLibrary[i].author}`;
@@ -82,25 +147,23 @@ pushBookButton.addEventListener("click", ()=>{
 
     bookCollection.appendChild(bookCard);
 
-    removeButtonPush.addEventListener("click", ()=>{
-      if(myLibrary[i].id===i){
-        delete myLibrary[i]; 
+    removeButtonPush.addEventListener("click", () => {
+      if (myLibrary[i].id === i) {
+        delete myLibrary[i];
         bookCollection.removeChild(bookCard);
         //myLibrary.splice(i,1);
         //console.log(myLibrary[i]);
       }
     });
 
-    readStatusPush.addEventListener("click", ()=>{
-      if(myLibrary[i].id===i){
-        if(readStatusPush.textContent == "Read"){
+    readStatusPush.addEventListener("click", () => {
+      if (myLibrary[i].id === i) {
+        if (readStatusPush.textContent == "Read") {
           readStatusPush.textContent = "Not read";
-        }else if (readStatusPush.textContent == "Not read") {
+        } else if (readStatusPush.textContent == "Not read") {
           readStatusPush.textContent = "Read";
         }
       }
     });
   }
 });
-
-
